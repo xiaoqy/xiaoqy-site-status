58332097d4295c3815e8746e94c385561662d7ad
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-07 08:27:06 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,37 +12,19 @@ const Footer = () => {
 
   return (
     <footer id="footer">
-      <div className="social">
-        <CustomLink
-          iconDom={<GithubOne />}
-          to={`https://github.com/${githubName}/`}
-        />
         <CustomLink iconDom={<Home />} to={homeUrl} />
         <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
       </div>
       <div className="text">
-        <p>
-          <CustomLink
-            text={Package.alia}
-            to="https://github.com/imsyy/site-status"
-          />
-          &nbsp;Version&nbsp;{Package.version}
-        </p>
         <p>
           基于&nbsp;
           <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
         </p>
         <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
+          Copyright&nbsp;&copy;&nbsp;2024&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
-          {siteIcp ? (
-            <React.Fragment>
-              &nbsp;|&nbsp;
-              <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
-            </React.Fragment>
-          ) : null}
+          <CustomLink to="https://www.xiaoqy.com/" text="白小七羽" />
         </p>
       </div>
     </footer>
